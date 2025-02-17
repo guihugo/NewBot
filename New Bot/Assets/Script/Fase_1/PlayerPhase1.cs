ec89b95a828bfaa64b8cd023379454bddb4f12d4
guihugo <guilherme.hugo@aluno.ufabc.edu.br>
2025-02-17 10:25:52 -0300

    MoveSmoothly

--- New Bot/Assets/Script/Fase_1/PlayerPhase1.cs
+++ New Bot/Assets/Script/Fase_1/PlayerPhase1.cs
@@ -9,6 +9,9 @@ public class PlayerPhase1 : MonoBehaviour
 
     public int movementDirection;
     public float gridSize;
+    private bool isMoving = false;
+    public float moveSpeed = 5f;
+
     private void Start()
     {
         GameManager gameManager = FindObjectOfType<GameManager>();
@@ -17,27 +20,61 @@ public class PlayerPhase1 : MonoBehaviour
 
     public void MoveCharacter(int index)
     {
-        if (index == 1)
+        if (!isMoving)
         {
-            transform.Translate(Vector3.left * movementDirection * gridSize);
-        }
-        else if (index == 2)
-        {
-            transform.Translate(Vector3.right * movementDirection * gridSize);
-        }
-        else if (index == 3)
-        {
-            transform.Translate(Vector3.up * -movementDirection * gridSize);
-        }
+            Vector3 moveVector = Vector3.zero;
 
-        else if (index == 4)
-        {
-            transform.Translate(Vector3.up * movementDirection * gridSize);
+            if (index == 1)
+            {
+                moveVector = Vector3.left * movementDirection;
+            }
+            else if (index == 2)
+            {
+                moveVector = Vector3.right * movementDirection;
+            }
+            else if (index == 3)
+            {
+                moveVector = Vector3.down * movementDirection;
+            }
+            else if (index == 4)
+            {
+                moveVector = Vector3.up * movementDirection;
+            }
+
+            if (moveVector != Vector3.zero)
+            {
+                StartCoroutine(MoveSmoothly(moveVector));
+            }
         }
     }
+
+    private IEnumerator MoveSmoothly(Vector3 direction)
+    {
+        isMoving = true;
+
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = startPosition + direction * gridSize;
+
+        float elapsedTime = 0f;
+        float duration = gridSize / moveSpeed;
+
+        while (elapsedTime < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        isMoving = false;
+    }
+
+
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision");
+        StopAllCoroutines();
+        isMoving = false;
     }
 }
