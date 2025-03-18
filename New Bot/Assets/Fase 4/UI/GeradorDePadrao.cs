d724bbbea1f2912a787d5c2dc6b85199b40bf1a4
Ian Nicolas Magatti da Silva <nicolas.magatti@gmail.com>
2025-03-17 22:02:02 -0300

    Avanços na UI
    Com base nos código do Guilherme para Fase 1, fiz alterações para o trabalho que estava realizando e consegui fazer com que os conteiner aparecam na tela do canvas de maneira organizada.

--- New Bot/Assets/Fase 4/UI/GeradorDePadrao.cs
+++ New Bot/Assets/Fase 4/UI/GeradorDePadrao.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Inventory.UI;
 using UnityEngine;
 
 public class GeradorDePadrao : MonoBehaviour
 {
     public int sequencia;
+    public GameObject itemPrefab;
+    public RectTransform contentPanel;
+    public List<GameObject> listOfUItems = new List<GameObject>();
+    public int cont;
+    private float x = -180, y = 124;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        contentPanel = GetComponent<RectTransform>();
+        InitializeInventoryUI(cont);
     }
 
     // Update is called once per frame
@@ -17,4 +25,26 @@ public class GeradorDePadrao : MonoBehaviour
     {
         
     }
+    public void InitializeInventoryUI(int inventorySize)
+    {
+        for (int i = 1; i < inventorySize; i++)
+        {
+            GameObject uiItem = Instantiate(itemPrefab, contentPanel.gameObject.transform); // Define como filho do contentPanel
+            RectTransform rectTransform = uiItem.GetComponent<RectTransform>();
+
+            // Ajuste da posição e escala corretamente para UI
+            rectTransform.anchoredPosition = new Vector2(x,y);
+            rectTransform.localScale = Vector3.one;
+
+
+            listOfUItems.Add(uiItem);
+            x += 180;
+            if( i%3 == 0 )
+            {
+                x = -180;
+                y -= 124;
+            }
+        }
+    }
+
 }
